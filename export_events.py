c0b414e9a145b652ac6f050b2a3037bcf3b74394
Charandas Batra-Daitch <charandas108@gmail.com>
2015-09-01 01:03:58 -0400

    Add mixpanel data filtering as well as map timestamp to keen friendly property

--- export_events.py
+++ export_events.py
@@ -9,6 +9,8 @@ from lib.mixpanel import Mixpanel
 from os.path import join, dirname
 from dotenv import load_dotenv
 
+from datetime import date
+
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
@@ -20,10 +22,22 @@ lines = api.request(['export'], {
     'from_date': '2015-08-01',
     'to_date': '2015-08-7',
     'event' : ['ad-click'],
-    'where': 'properties["network"] == "chi"'
+    'where': 'properties["network"] == "chi"'    
 })
 
+# print(lines)
+
 for line in lines:
-    properties = json.loads(line)
-    print(properties)
+    metadata = json.loads(line)
+        
+    properties = { key: metadata['properties'][key] \
+        for key, value in metadata['properties'].iteritems() \
+        if not key.startswith('$')}
+
+    timestamp = properties['time']    
+    properties['keen'] = {}
+    properties['keen']['timestamp'] = date.fromtimestamp(timestamp).isoformat()
+
+    properties['event'] = metadata['event']
+
     keen.add_event('core', properties)
